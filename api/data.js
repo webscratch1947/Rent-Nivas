37ffad778249096521845af47020f4d9cb3ea9b3
Web Scratch <webscratch99@gmail.com>
2026-06-30 09:15:04 +0530

    Update data.js

--- api/data.js
+++ api/data.js
@@ -7,7 +7,7 @@ const { CognitoIdentityProviderClient, ListUsersCommand, AdminDeleteUserCommand 
 
 const REGION = process.env.AWS_REGION || process.env.RENT_NIVAS_AWS_REGION || 'eu-north-1';
 const USER_POOL_ID = process.env.COGNITO_USER_POOL_ID;
-const APP_CLIENT_ID = process.env.COGNITO_CLIENT_ID;
+const APP_CLIENT_ID = process.env.COGNITO_APP_CLIENT_ID;
 const ISSUER = `https://cognito-idp.${REGION}.amazonaws.com/${USER_POOL_ID}`;
 const JWKS_URL = `${ISSUER}/.well-known/jwks.json`;
 
